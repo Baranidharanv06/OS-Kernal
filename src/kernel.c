40a675017cab0ddb50c6e75a95ca189b3d700357
Baranidharan Viswanathan <pilgarlic@Baranidharans-MacBook-Air.local>
2026-03-28 14:31:32 +0530

    creating a simple kernel

---
+++ src/kernel.c
@@ -0,0 +1,9 @@
+   // kernel.c                                                                                                                                                                              
+   void kernel_main() {                                                                                                                                                                     
+   const char *message = "Hello from my kernel!";                                                                                                                                           
+   char *video_memory = (char *) 0xb8000;                                                                                                                                                   
+   for (int i = 0; message[i] != '\0'; i++) {                                                                                                                                               
+   video_memory[i * 2] = message[i];                                                                                                                                                        
+   video_memory[i * 2 + 1] = 0x07; // Light grey on black                                                                                                                                   
+   }                                                                                                                                                                                        
+   }   
