30ea53393e3a6cd456d16168c4f22062d4c19d63
Baranidharan Viswanathan <pilgarlic@Baranidharans-MacBook-Air.local>
2026-03-28 16:53:40 +0530

    updated kernal for keyboard driver

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,18 @@
-   // kernel.c                                                                                                                                                                              
-   void kernel_main() {                                                                                                                                                                     
-   const char *message = "Hello from my kernel!";                                                                                                                                           
-   char *video_memory = (char *) 0xb8000;                                                                                                                                                   
-   for (int i = 0; message[i] != '\0'; i++) {                                                                                                                                               
-   video_memory[i * 2] = message[i];                                                                                                                                                        
-   video_memory[i * 2 + 1] = 0x07; // Light grey on black                                                                                                                                   
-   }                                                                                                                                                                                        
-   }   
+extern void keyboard_handler();
+
+void kernel_main() {
+    // Clear screen
+    char* video = (char*)0xb8000;
+    for (int i = 0; i < 80*25*2; i++) video[i] = 0;
+
+    // Print welcome message
+    const char* msg = "Kernel ready! Press keys:";
+    for (int i = 0; msg[i]; i++) {
+        video[i*2] = msg[i];
+        video[i*2+1] = 0x07;
+    }
+
+    // Enable interrupts and wait
+    __asm__ volatile ("sti");
+    while(1);
+}
