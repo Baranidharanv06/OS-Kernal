68d833c5e7908f36e439b67354eb1b23d37541ac
Baranidharanv06 <baranidharanviswanathan2006@gmail.com>
2026-03-29 13:32:22 +0530

    video is defined in both shell.c and panic.c, so fixing it by removing duplicats from panic.c using extern

--- src/panic.c
+++ src/panic.c
@@ -1,15 +1,6 @@
 #include <stdint.h>
 
-volatile char* video = (volatile char*)0xb8000;
-
-void panic_print_str(const char* s, int* col, int row, uint8_t color) {
-    for (int i = 0; s[i]; i++) {
-        int pos = (row * 80 + *col);
-        video[pos * 2]     = s[i];
-        video[pos * 2 + 1] = color;
-        (*col)++;
-    }
-}
+extern volatile char* video;
 
 void panic_fill_row(int row, char c, uint8_t color) {
     for (int col = 0; col < 80; col++) {
@@ -19,34 +10,37 @@ void panic_fill_row(int row, char c, uint8_t color) {
     }
 }
 
+void panic_print_str(const char* s, int* col, int row, uint8_t color) {
+    for (int i = 0; s[i]; i++) {
+        int pos = row * 80 + *col;
+        video[pos * 2]     = s[i];
+        video[pos * 2 + 1] = color;
+        (*col)++;
+    }
+}
+
 void kernel_panic(const char* exception, uint32_t code) {
-    // fill entire screen dark red
+    // fill entire screen red
     for (int i = 0; i < 80 * 25; i++) {
         video[i * 2]     = ' ';
-        video[i * 2 + 1] = 0x40; // red background
+        video[i * 2 + 1] = 0x40;
     }
 
     // top bar
-    panic_fill_row(0, ' ', 0x4F); // red bg white fg
-    int col = 1;
-    panic_print_str("  KERNEL PANIC  ", &col, 0, 0x4F);
-
-    // blank row
-    panic_fill_row(2, ' ', 0x40);
+    panic_fill_row(0, ' ', 0x4F);
+    int col = 2;
+    panic_print_str("*** KERNEL PANIC ***", &col, 0, 0x4F);
 
     // exception name
     col = 4;
-    panic_print_str("Exception: ", &col, 4, 0x4E); // yellow
-    panic_print_str(exception,    &col, 4, 0x4F); // white
+    panic_print_str("Exception: ", &col, 4, 0x4E);
+    panic_print_str(exception,    &col, 4, 0x4F);
 
     // error code
     col = 4;
     panic_print_str("Error code: 0x", &col, 6, 0x4E);
-
-    // print hex error code
     const char* hex = "0123456789ABCDEF";
-    char buf[9];
-    buf[8] = '\0';
+    char buf[9]; buf[8] = '\0';
     for (int i = 7; i >= 0; i--) {
         buf[i] = hex[code & 0xF];
         code >>= 4;
@@ -55,16 +49,15 @@ void kernel_panic(const char* exception, uint32_t code) {
 
     // message
     col = 4;
-    panic_print_str("The kernel has encountered a fatal error.", &col, 8, 0x4C);
+    panic_print_str("A fatal error occurred. System halted.", &col, 8, 0x4C);
     col = 4;
-    panic_print_str("System halted. Please restart QEMU.", &col, 9, 0x4C);
+    panic_print_str("Restart QEMU to reboot.", &col, 9, 0x4C);
 
     // bottom bar
     panic_fill_row(24, ' ', 0x4F);
-    col = 1;
-    panic_print_str("  MyKernel v1.0  |  github.com/Baranidharanv06/OS-Kernal", &col, 24, 0x4F);
+    col = 2;
+    panic_print_str("MyKernel v2.0  |  github.com/Baranidharanv06/OS-Kernal", &col, 24, 0x4F);
 
-    // halt forever
     __asm__ volatile ("cli; hlt");
     while(1);
 }
