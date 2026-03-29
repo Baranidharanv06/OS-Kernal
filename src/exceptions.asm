ad012be301b9a73204a3276aa3a864b66ec95486
Baranidharanv06 <baranidharanviswanathan2006@gmail.com>
2026-03-29 13:40:01 +0530

    v1.6 updating exception assembly to capture register and pass them

--- src/exceptions.asm
+++ src/exceptions.asm
@@ -1,33 +1,29 @@
 [bits 32]
-extern kernel_panic
+extern kernel_panic_full
 
 global isr0
 global isr13
 global isr14
 
-isr0:
+%macro ISR_HANDLER 2
     pusha
-    push dword 0
-    push dword isr0_msg
-    call kernel_panic
+    mov eax, esp        ; pointer to pushed registers
+    push dword %2       ; error code
+    push dword %1       ; exception name string
+    push eax            ; registers pointer
+    call kernel_panic_full
     popa
     iret
+%endmacro
+
+isr0:
+    ISR_HANDLER isr0_msg, 0
 
 isr13:
-    pusha
-    push dword 0
-    push dword isr13_msg
-    call kernel_panic
-    popa
-    iret
+    ISR_HANDLER isr13_msg, 0
 
 isr14:
-    pusha
-    push dword 0
-    push dword isr14_msg
-    call kernel_panic
-    popa
-    iret
+    ISR_HANDLER isr14_msg, 0
 
 isr0_msg:  db "Division by Zero (Exception #0)", 0
 isr13_msg: db "General Protection Fault (Exception #13)", 0
